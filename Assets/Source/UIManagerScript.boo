92e7a1aa0e739e42d845773f3aa6fba641893107
darkoverlordofdata <darkoverlordofdata@gmail.com>
2015-12-12 23:27:32 -0800

    daily

---
+++ Assets/Source/UIManagerScript.boo
@@ -0,0 +1,11 @@
+import UnityEngine
+		
+class UIManagerScript(MonoBehaviour):
+	# Use this for initialization
+	def Start ():
+		pass
+	
+	# Update is called once per frame
+	def Update ():
+		pass
+
---
+++ Assets/Source/UIManagerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManagerScript : MonoBehaviour {
+
+	public void Start() {
+		Debug.Log ("Hello");
+	}
+
+	public void StartGame() {
+		Application.LoadLevel("Game Scene");
+	}
+
+	public void LoadLeaderboard() {
+		Application.LoadLevel("LeaderboardScene");
+	}
+
+	public void LoadCredits() {
+		Application.LoadLevel("CreditsScene");
+	}
+	
+	public void LoadMenu() {
+		Application.LoadLevel("MenuScene");
+	}
+
+}
